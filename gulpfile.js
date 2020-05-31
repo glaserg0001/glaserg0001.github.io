9d62c89e69495077c3da5823e7d83740de5bcc7c
glaserg001 <glaserg001@gmail.com>
2020-05-31 16:19:17 +0300

    add editor config

--- gulpfile.js
+++ gulpfile.js
@@ -1,103 +1,103 @@
-var gulp					= require('gulp'),
-		sass					= require('gulp-sass'),
-		browserSync		= require('browser-sync'),
-		del						= require('del'),
-		imagemin			= require('gulp-imagemin'),
-		pngquant			= require('imagemin-pngquant'),
-		cache					= require('gulp-cache'),
-		include				= require('gulp-html-tag-include'),
-		sourcemaps		= require('gulp-sourcemaps'),
-		autoprefixer	= require('gulp-autoprefixer');
+var gulp            = require('gulp'),
+sass            = require('gulp-sass'),
+browserSync		= require('browser-sync'),
+del             = require('del'),
+imagemin        = require('gulp-imagemin'),
+pngquant        = require('imagemin-pngquant'),
+cache           = require('gulp-cache'),
+include         = require('gulp-html-tag-include'),
+sourcemaps      = require('gulp-sourcemaps'),
+autoprefixer    = require('gulp-autoprefixer');
 
 
 var app = './app'
 
 var sassFun = function(){
-	return gulp.src(app + '/sass/*.+(scss|sass)')
-	.pipe(sourcemaps.init())
-	.pipe( sass().on( 'error', function( error ){console.log( error ); }))
-	.pipe(sass({outputStyle: 'expanded'}).on('error', sass.logError))
-	// .pipe(sass())
-	.pipe(autoprefixer(['last 10 versions', '> 1%'], { cascade: true }))
-	.pipe(sourcemaps.write('../maps', {addComment: false}))
-	.pipe(gulp.dest(app + '/css'))
-	.pipe(browserSync.reload({stream: true}))
+    return gulp.src(app + '/sass/*.+(scss|sass)')
+    .pipe(sourcemaps.init())
+    .pipe( sass().on( 'error', function( error ){console.log( error ); }))
+    .pipe(sass({outputStyle: 'expanded'}).on('error', sass.logError))
+    // .pipe(sass())
+    .pipe(autoprefixer(['last 2 versions', '> 1%'], { cascade: true }))
+    .pipe(sourcemaps.write('../maps', {addComment: false}))
+    .pipe(gulp.dest(app + '/css'))
+    .pipe(browserSync.reload({stream: true}))
 }
 
 
 gulp.task('sass', sassFun);
 
 gulp.task('browser-sync', function() {
-	browserSync({
-		server: {
-			baseDir: './app'
-		},
-		notify: false,
-		// ghostMode: false,
-	});
+    browserSync({
+        server: {
+            baseDir: './app'
+        },
+        notify: false,
+        // ghostMode: false,
+    });
 });
 
 
 gulp.task('html-include', function() {
-	return gulp.src([app + '/html/*.html', app + '/html/tutorials/*.html'])
-	.pipe(include())
-	.pipe(gulp.dest('./app'))
-	.pipe(browserSync.reload({ stream: true }));
+    return gulp.src([app + '/html/*.html', app + '/html/tutorials/*.html'])
+    .pipe(include())
+    .pipe(gulp.dest('./app'))
+    .pipe(browserSync.reload({ stream: true }));
 });
 
 
 gulp.task('watch', ['browser-sync'], function() {
-	// gulp.watch(app + '/sass/**/*.sass', ['sass']);
-
-	gulp.watch(app + '/sass/*.+(scss|sass)', function () {
-		setTimeout (function () { 
-			sassFun ()
-		}, 500);
-	});
-
-
-	gulp.watch(app + '/html/**/*.html', ['html-include']);
-
-	// gulp.watch('./app/**/*.html', browserSync.reload);
-	gulp.watch(app + '/js/**/*.js', browserSync.reload);
+    // gulp.watch(app + '/sass/**/*.sass', ['sass']);
+    
+    gulp.watch(app + '/sass/*.+(scss|sass)', function () {
+        setTimeout (function () { 
+            sassFun ()
+        }, 500);
+    });
+    
+    
+    gulp.watch(app + '/html/**/*.html', ['html-include']);
+    
+    // gulp.watch('./app/**/*.html', browserSync.reload);
+    gulp.watch(app + '/js/**/*.js', browserSync.reload);
 });
 
 gulp.task('clean', function() {
-	return del.sync('dist');
+    return del.sync('dist');
 });
 
 gulp.task('img', function() {
-	return gulp.src(app + '/img/**/*')
-	.pipe(cache(imagemin({
-		// .pipe(imagemin({
-			interlaced: true,
-			progressive: true,
-			svgoPlugins: [{removeViewBox: false}],
-			use: [pngquant()]
-		})))
-	.pipe(gulp.dest('dist/img'));
+    return gulp.src(app + '/img/**/*')
+    .pipe(cache(imagemin({
+        // .pipe(imagemin({
+        interlaced: true,
+        progressive: true,
+        svgoPlugins: [{removeViewBox: false}],
+        use: [pngquant()]
+    })))
+    .pipe(gulp.dest('dist/img'));
 });
 
 gulp.task('build', ['clean', 'img', 'sass'], function() {
-
-	var buildCss = gulp.src([
-		app + '/css/*.css'
-		])
-	.pipe(gulp.dest('dist/css'))
-
-	var buildFonts = gulp.src(app + '/fonts/**/*')
-	.pipe(gulp.dest('dist/fonts'))
-
-	var buildJs = gulp.src(app + '/js/**/*')
-	.pipe(gulp.dest('dist/js'))
-
-	var buildHtml = gulp.src(app + '/*.html')
-	.pipe(gulp.dest('dist'));
-
+    
+    var buildCss = gulp.src([
+        app + '/css/*.css'
+    ])
+    .pipe(gulp.dest('dist/css'))
+    
+    var buildFonts = gulp.src(app + '/fonts/**/*')
+    .pipe(gulp.dest('dist/fonts'))
+    
+    var buildJs = gulp.src(app + '/js/**/*')
+    .pipe(gulp.dest('dist/js'))
+    
+    var buildHtml = gulp.src(app + '/*.html')
+    .pipe(gulp.dest('dist'));
+    
 });
 
 gulp.task('clear', function (callback) {
-	return cache.clearAll();
+    return cache.clearAll();
 })
 
 gulp.task('default', ['watch']);
