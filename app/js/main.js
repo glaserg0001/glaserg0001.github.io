fadd1bc990da045d07af38c3f84703593e01e6a9
Serhii Hladkyi <s.hladkyi@astoundcommerce.com>
2020-10-02 19:56:00 +0300

    mult in progress

--- app/js/main.js
+++ app/js/main.js
@@ -1,2 +1,1 @@
 // common js
-console.log('main js');
--- app/js/pages/ideas/multiplication.js
+++ app/js/pages/ideas/multiplication.js
@@ -1,13 +1,45 @@
-var btn = document.querySelector('.js-btn');
+let btn = document.querySelector('.js-btn');
+let result = document.querySelector('.js-result');
 
 btn.addEventListener('click', submit);
 
 function submit() {
-    var inputList = document.querySelectorAll('.js-input');
+    let inputList = document.querySelectorAll('.js-input');
+
+    let arr = [];
+
+    let resultHtmlWrap = document.createElement('div');
+    resultHtmlWrap.classList.add('result-wrap');
 
     inputList.forEach((el) => {
-        console.log(parseInt(el.value))
-    })
+        let val = el.value;
+        let valArr = el.value.split('');
 
-    console.log(inputList)
+        arr.push(valArr);
+
+        // console.log(val)
+    });
+    
+    for (let i = 0; i < arr.length; i++) {
+        const subArr = arr[i];
+
+        let resultHtmlHeadItem = document.createElement('ul');
+        resultHtmlHeadItem.classList.add('result-head-item');
+
+        // console.log( subArr.split('') );
+        
+        if ( subArr.length ) {
+            for (let j = 0; j < subArr.length; j++) {
+                let resultHtmlHeadItemNum = document.createElement('li');
+                resultHtmlHeadItemNum.innerText = subArr[j];
+                resultHtmlHeadItem.appendChild(resultHtmlHeadItemNum);
+            }
+
+            resultHtmlWrap.appendChild(resultHtmlHeadItem);
+        }
+    }
+
+    result.appendChild(resultHtmlWrap);
+
+    console.log(arr)
 }
