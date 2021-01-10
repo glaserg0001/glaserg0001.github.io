20df5ea483b342fb40748465fb420b749cd65e16
glaserg001 <glaserg001@gmail.com>
2021-01-11 01:35:05 +0200

    Snake move start

--- app/js/pages/ideas/snake.js
+++ app/js/pages/ideas/snake.js
@@ -7,9 +7,10 @@ function Snake(id, size, snakeSizeBase) {
     this.snakeSizeBase = snakeSizeBase;
     this.snakeBody = [];
     this.coords = []; // y - 1st level; x - 2nd level; e.g. this.coords[y][x]
+    this.error = 'Please enter less value for the snake size';
 
     if (this.snakeSizeBase >= this.x) {
-        this.container.innerHTML = '<div style="color: red;">Please enter less value for the size of the snake</div>'
+        this.container.innerHTML = `<div style="color: red;">${this.error}</div>`
         return false
     }
 
@@ -49,12 +50,47 @@ function Snake(id, size, snakeSizeBase) {
         }
     }
 
+    
+
+    // Snake move
+    this.snakeMove = function () {
+        let interval = setInterval(() => {
+            // debugger
+            const snakeHeadX = this.snakeBody[0][0];
+            const snakeHeadY = this.snakeBody[0][1];
+            const snakeHeadHTML = this.coords[snakeHeadY][snakeHeadX];
+            // remove class m-head from head of Snake
+            snakeHeadHTML.classList.remove('m-head');
+            
+            // right START
+            // add new cell
+            const snakeHeadXNext = (snakeHeadX == this.x - 1) ? 0 : snakeHeadX + 1;
+            // console.log(snakeHeadXNext);
+            // console.log(snakeHeadX, snakeHeadY);
+            this.snakeBody.unshift([snakeHeadXNext, snakeHeadY]);
+            // add class for a new cell
+            this.coords[snakeHeadY][snakeHeadXNext].classList.add('snake-body', 'm-head');
+            // remove class for old cell (last)
+            const snakeBodyLast = this.snakeBody[this.snakeBody.length - 1];
+            const snakeBodyLastX = snakeBodyLast[0];
+            const snakeBodyLastY = snakeBodyLast[1];
+            this.coords[snakeBodyLastY][snakeBodyLastX].classList.remove('snake-body');
+            // remove old cell (last)
+            this.snakeBody.pop()
+
+            // console.log(snakeBodyLastX);
+            console.log(this.snakeBody)
+            
+            // right END
+        }, 2000);
+    }
+
     // ==== Header
     this.header = function () {
         const _header = document.createElement('div');
         _header.classList.add(`${blockName}-header`);
 
-        _header.innerText = 'Header'
+        _header.innerHTML = 'Points: 0 <span style="float: right">Best Result: 0</span>'
 
         // append header block
         this.container.append(_header);
@@ -70,8 +106,8 @@ function Snake(id, size, snakeSizeBase) {
             for (let j = 0; j < this.x; j++) {
                 const _cell = document.createElement('i');
                 _cell.classList.add(`${blockName}-middle__cell`, `js-${blockName}-cell`);
-                _cell.setAttribute('data-x', j);
-                _cell.setAttribute('data-y', i);
+                // _cell.setAttribute('data-x', j);
+                // _cell.setAttribute('data-y', i);
                 _middle.append(_cell);
 
                 _coordsRow.push(_cell);
@@ -94,7 +130,17 @@ function Snake(id, size, snakeSizeBase) {
         const _footer = document.createElement('div');
         _footer.classList.add(`${blockName}-footer`);
 
-        _footer.innerText = 'Footer'
+        const btnStart = document.createElement('button');
+        const btnReset = document.createElement('button');
+
+        btnStart.classList.add('button1');
+        btnReset.classList.add('button2');
+        btnReset.style.float = 'right';
+        btnStart.innerText = 'Start';
+        btnReset.innerText = 'Reset';
+
+
+        _footer.append(btnStart, btnReset);
 
         // append footer block
         this.container.append(_footer);
@@ -103,6 +149,8 @@ function Snake(id, size, snakeSizeBase) {
         this.header();
         this.middle();
         this.footer();
+
+        this.snakeMove();
     }
     this.main()
 }
@@ -112,3 +160,8 @@ let snake = new Snake(
         size = [10, 12],
         snakeSizeBase = 3
     )
+
+
+
+// notes
+// https://www.educative.io/blog/javascript-snake-game-tutorial
