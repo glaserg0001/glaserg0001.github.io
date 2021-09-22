22d32eb825ad6912b314586758592c6ba85c0343
glaserg0001 <glaserg001@gmail.com>
2021-09-22 18:15:49 +0300

    drag and drop in progress

--- app/js/pages/ideas/sea_battle.js
+++ app/js/pages/ideas/sea_battle.js
@@ -256,21 +256,30 @@ console.log(seaBattle)
 console.log(seaBattle.element.ships)
 
 // https://learn.javascript.ru/mouse-drag-and-drop
+ball.ondragstart = function() {
+    return false;
+};
+
 ball.onmousedown = function(event) {
+    let shiftX = event.clientX - ball.getBoundingClientRect().left;
+    let shiftY = event.clientY - ball.getBoundingClientRect().top;
+
     ball.style.position = 'absolute';
     ball.style.zIndex = 1000;
     document.body.append(ball);
 
     console.log(event)
+
     
     moveAt(event.pageX, event.pageY);
     
     function moveAt(pageX, pageY) {
-        ball.style.left = pageX - ball.offsetWidth / 2 + 'px';
-        ball.style.top = pageY - ball.offsetHeight / 2 + 'px';
+        ball.style.left = pageX - shiftX + 'px';
+        ball.style.top = pageY - shiftY + 'px';
     }
     
     function onMouseMove(event) {
+        event.preventDefault()
         moveAt(event.pageX, event.pageY);
     }
     
